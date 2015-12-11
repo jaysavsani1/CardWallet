e12251eaeb36e1e107b1a7a43defc223eccc72f3
Jay Savsani <jay@quixom.com>
2015-12-11 21:07:15 +0530

    list card view layout added.

---
+++ app/src/main/java/com/quixom/cardwallet/Utils/CardInfo.java
@@ -0,0 +1,56 @@
+package com.quixom.cardwallet.Utils;
+
+/**
+ * Created by Admin on 11-Dec-15.
+ */
+public class CardInfo {
+    String category, cardType, cardNumber, cardName, cardExpiry, cardCVV;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getCardExpiry() {
+        return cardExpiry;
+    }
+
+    public void setCardExpiry(String cardExpiry) {
+        this.cardExpiry = cardExpiry;
+    }
+
+    public String getCardCVV() {
+        return cardCVV;
+    }
+
+    public void setCardCVV(String cardCVV) {
+        this.cardCVV = cardCVV;
+    }
+}
