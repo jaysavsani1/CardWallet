21aa1e4c9fa91d6547caa2046ae464a9533cd815
BhumaJ <jay.savsani07@gmail.com>
2015-12-16 19:24:09 +0530

    list card view layout file needs some changes. 1) add name below expiry date

--- app/src/main/java/com/quixom/cardwallet/SplashActivity.java
+++ app/src/main/java/com/quixom/cardwallet/SplashActivity.java
@@ -1,8 +1,13 @@
 package com.quixom.cardwallet;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,13 +15,19 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.card_view_layout);
+
+        TextView textView = (TextView) findViewById(R.id.cvl_tv_card_number);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "cc.ttf");
+        textView.setTypeface(tf);
+
+
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(5000);
                     Intent intent = new Intent(SplashActivity.this, ListOfCardsActivity.class);
                     startActivity(intent);
                 } catch (InterruptedException e) {
